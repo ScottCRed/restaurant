fa372e2eef26fcf285a047416fd939ca7a89354c
ScottCRed <ScottC@redpandasoftware.co.za>
2023-01-31 13:18:13 +0200

    Remove unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+//import createMainPage from "./open-page";
+
+const container = document.querySelector('#container')
+const btn = document.createElement('button');
+btn.classList.add('press')
+btn.textContent = 'press'
+container.appendChild(btn)
+
+const addClick = document.querySelector('.press')
+addClick.addEventListener('click', createMainPage )
+
 console.log('working again');
 
