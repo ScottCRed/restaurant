c34524f720a9927c62f181419a8c5cb28d77d4e7
ScottCRed <ScottC@redpandasoftware.co.za>
2023-01-31 09:47:12 +0200

    Adjust environemnt to support dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+      },
+    output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    plugins: [
+        new HtmlWebpackPlugin({
+         title: 'Development',
+        }),
+      ],
+      output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+      },
+      optimization: {
+        runtimeChunk: 'single',
+      },
+},
 };
