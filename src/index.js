9aaf062411883b1d39b2c6366b597343997d8892
ScottCRed <ScottC@redpandasoftware.co.za>
2023-02-01 12:47:06 +0200

    Create mouse over listener to change button color dynamically

--- src/index.js
+++ src/index.js
@@ -12,12 +12,18 @@ mainPage();
 const page1Click = document.querySelector('.mainP');
     page1Click.addEventListener('click', clear );
     page1Click.addEventListener('click', mainPage);
+    page1Click.addEventListener('mouseover', () => page1Click.style.backgroundColor = 'black');
+    page1Click.addEventListener('mouseout', () => page1Click.style.backgroundColor = 'transparent');
     
 
 const page2Click = document.querySelector('.offerP');
     page2Click.addEventListener('click', clear);
     page2Click.addEventListener('click', page2);
+    page2Click.addEventListener('mouseover', () => page2Click.style.backgroundColor = 'black');
+    page2Click.addEventListener('mouseout', () => page2Click.style.backgroundColor = 'transparent');
 
 const page3Click = document.querySelector('.contactP');
     page3Click.addEventListener('click', clear);
     page3Click.addEventListener('click', page3);
+    page3Click.addEventListener('mouseover', () => page3Click.style.backgroundColor = 'black');
+    page3Click.addEventListener('mouseout', () => page3Click.style.backgroundColor = 'transparent');
