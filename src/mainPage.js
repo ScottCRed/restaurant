d60963cbe7d229490ef8b60e5b2684b104264e30
ScottCRed <ScottC@redpandasoftware.co.za>
2023-02-01 11:51:18 +0200

    Create content template for mainPage

--- src/mainPage.js
+++ src/mainPage.js
@@ -1,11 +1,29 @@
 import './style.css'
+import Main from '/img/main.jpg'
 
 export function mainPage () {
     
     const container = document.querySelector('#container');
     const backing = document.createElement('div');
     backing.classList.add('page');
-    backing.textContent = "push me bih";
+    
+    const pageHeading = document.createElement('h2');
+    pageHeading.textContent = 'Main page!'
+
+    const img = document.createElement('img');
+    img.src = Main
+
+    const para = document.createElement('p');
+    para.textContent = 'If this was a real paragraph I might actually ahve something to say \n \
+    If this was a real paragraph I might actually ahve something to say \n \
+    If this was a real paragraph I might actually ahve something to say \n \
+    If this was a real paragraph I might actually ahve something to say \n \
+    If this was a real paragraph I might actually ahve something to say \n \
+    If this was a real paragraph I might actually ahve something to say'
+
+    backing.appendChild(pageHeading);
+    backing.appendChild(img);
+    backing.appendChild(para);
     
     container.appendChild(backing);
    
