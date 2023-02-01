f7b6b19673ffaf80731a8042cdb8b924907976fe
ScottCRed <ScottC@redpandasoftware.co.za>
2023-02-01 11:22:33 +0200

    Create clear function and add to click event for each button

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import { page2 } from "./page2";
 import { page3 } from "./page3";
 import './style.css'
 
+function clear() {
+    document.getElementById('container').innerHTML = '';
+    console.log('supposed to work')
+}
+
 mainPage();
 
 const page1Click = document.querySelector('.mainP');
+    page1Click.addEventListener('click', clear );
     page1Click.addEventListener('click', mainPage);
+    
 
 const page2Click = document.querySelector('.offerP');
+    page2Click.addEventListener('click', clear);
     page2Click.addEventListener('click', page2);
 
 const page3Click = document.querySelector('.contactP');
+    page3Click.addEventListener('click', clear);
     page3Click.addEventListener('click', page3);
--- src/mainPage.js
+++ src/mainPage.js
@@ -1,11 +1,14 @@
 import './style.css'
 
 export function mainPage () {
+    
     const container = document.querySelector('#container');
     const backing = document.createElement('div');
     backing.classList.add('page');
     backing.textContent = "push me bih";
+    
     container.appendChild(backing);
+   
     console.log('buttybutyy')
    
 }
